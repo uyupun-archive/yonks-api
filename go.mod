02db9316d6ab1c65721eb4720f82bcc83a9fc4ca
Kazuki Tobita <kazukiti201@gmail.com>
2021-11-10 23:18:43 +0900

    Modify auth login api

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.6.1
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 	gorm.io/driver/mysql v1.1.3
 	gorm.io/gorm v1.22.2
 )
--- handlers/auth.go
+++ handlers/auth.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/uyupun/yonks-api/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func AuthRegister(c echo.Context) error {
@@ -20,19 +21,33 @@ func AuthRegister(c echo.Context) error {
 }
 
 func AuthLogin(c echo.Context) error {
-	user := new(models.User)
-	err := c.Bind(&user)
+	loginInfo := struct {
+		UserID   string `json:"user_id"`
+		Password string `json:"password"`
+	}{}
+
+	err := c.Bind(&loginInfo)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	// TODO: ユーザIDとパスワードを検証する処理
-	if user.UserID != "takashi0602" || user.Password != "testtest" {
-		return c.JSON(http.StatusBadRequest, err)
+	password := []byte(loginInfo.Password)
+	hashed, err := bcrypt.GenerateFromPassword(password, 10)
+	if err != nil {
+		return c.JSON(http.StatusServiceUnavailable, err)
 	}
 
-	// TODO: トークン等を返す
-	return c.JSON(http.StatusOK, "")
+	// DBのユーザ情報を取得
+
+	// パスワードの検証
+	err = bcrypt.CompareHashAndPassword(hashed, password)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, err)
+	}
+
+	// トークンの生成
+
+	return c.JSON(http.StatusOK, nil)
 }
 
 func AuthLogout(c echo.Context) error {
